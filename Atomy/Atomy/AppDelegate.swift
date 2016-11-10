b6ae28255f6a595d915a271ded899ff552ce5194
markedwardmurray <markedwardmurray@gmail.com>
2016-11-09 22:35:42 -0500

    generate Xcode project

---
+++ Atomy/Atomy/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Atomy
+//
+//  Created by Mark Murray on 11/9/16.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
