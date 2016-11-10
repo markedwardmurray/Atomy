77585b842065b9b771850f8d0201c025507519e0
markedwardmurray <markedwardmurray@gmail.com>
2016-11-09 23:17:40 -0500

    [Models] Neutron, use ColorWithHex to compute UIColor from hex string

--- Atomy/Atomy/Models/Neutron.swift
+++ Atomy/Atomy/Models/Neutron.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import ColorWithHex
 
 struct Neutron: ColorConfigurable {
     let hexColor: HexColor
     var color: UIColor {
-        
+        return UIColor.colorWithHex(self.hexColor)!
+    }
+    
+    init(hexColor: HexColor) {
+        self.hexColor = hexColor;
     }
 
 }
